7c23a885a04f0ffed4187cb9321f7bdafa90bd30
Ronish <ronish.rohan@gmail.com>
2025-10-29 15:34:20 +0530

    fix: fix errors

--- app/renderer.tsx
+++ app/renderer.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
-import { ErrorBoundary } from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 )
